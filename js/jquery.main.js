2a0c721843daef0a12c3181dd393a3985ccd9458
Brendan Wovchko <wovchko@Wovchko-MBA-5.local>
2015-04-22 00:32:53 -0500

    New website

---
+++ js/jquery.main.js
@@ -0,0 +1,523 @@
+// page init
+jQuery(function(){
+	initAnchors();
+	initMobileNav();
+	initBackgroundResize();
+	initFitVids();
+});
+
+// mobile menu init
+function initMobileNav() {
+	jQuery('body').mobileNav({
+		hideOnClickOutside: true,
+		menuActiveClass: 'nav-active',
+		menuOpener: '.nav-opener',
+		menuDrop: '.main-nav',
+		onInit: function(self) {
+			var menu = self.drop;
+			var links = menu.find('a');
+
+			links.on('click', function() {
+				self.hide();
+			});
+		}
+	});
+}
+
+// initialize smooth anchor links
+function initAnchors() {
+	new SmoothScroll({
+		anchorLinks: '.nav a[href^="#"]:not([href="#"])',
+		extraOffset: 57,
+		onChange: function() {
+		}
+	});
+}
+
+// stretch background to fill blocks
+function initBackgroundResize() {
+	jQuery('.bg-stretch').each(function() {
+		ImageStretcher.add({
+			container: this,
+			image: 'img'
+		});
+	});
+}
+
+// handle flexible video size
+function initFitVids() {
+	jQuery('.video-holder').fitVids();
+}
+
+/*
+ * Simple Mobile Navigation
+ */
+;(function($) {
+	function MobileNav(options) {
+		this.options = $.extend({
+			container: null,
+			hideOnClickOutside: false,
+			menuActiveClass: 'nav-active',
+			menuOpener: '.nav-opener',
+			menuDrop: '.nav-drop',
+			toggleEvent: 'click',
+			outsideClickEvent: 'click touchstart pointerdown MSPointerDown'
+		}, options);
+		this.initStructure();
+		this.attachEvents();
+	}
+	MobileNav.prototype = {
+		initStructure: function() {
+			this.page = $('html');
+			this.container = $(this.options.container);
+			this.opener = this.container.find(this.options.menuOpener);
+			this.drop = this.container.find(this.options.menuDrop);
+			this.makeCallback('onInit', this);
+		},
+		attachEvents: function() {
+			var self = this;
+
+			if(activateResizeHandler) {
+				activateResizeHandler();
+				activateResizeHandler = null;
+			}
+
+			this.outsideClickHandler = function(e) {
+				if(self.isOpened()) {
+					var target = $(e.target);
+					if(!target.closest(self.opener).length && !target.closest(self.drop).length) {
+						self.hide();
+					}
+				}
+			};
+
+			this.openerClickHandler = function(e) {
+				e.preventDefault();
+				self.toggle();
+			};
+
+			this.opener.on(this.options.toggleEvent, this.openerClickHandler);
+		},
+		isOpened: function() {
+			return this.container.hasClass(this.options.menuActiveClass);
+		},
+		show: function() {
+			this.container.addClass(this.options.menuActiveClass);
+			if(this.options.hideOnClickOutside) {
+				this.page.on(this.options.outsideClickEvent, this.outsideClickHandler);
+			}
+		},
+		hide: function() {
+			this.container.removeClass(this.options.menuActiveClass);
+			if (this.options.hideOnClickOutside) {
+				this.page.off(this.options.outsideClickEvent, this.outsideClickHandler);
+			}
+			// this.makeCallback('onChange', this);
+		},
+		toggle: function() {
+			if(this.isOpened()) {
+				this.hide();
+			} else {
+				this.show();
+			}
+		},
+		makeCallback: function(name) {
+			if (typeof this.options[name] === 'function') {
+				var args = Array.prototype.slice.call(arguments);
+				args.shift();
+				this.options[name].apply(this, args);
+			}
+		},
+		destroy: function() {
+			this.container.removeClass(this.options.menuActiveClass);
+			this.opener.off(this.options.toggleEvent, this.clickHandler);
+			this.page.off(this.options.outsideClickEvent, this.outsideClickHandler);
+		}
+	};
+
+	var activateResizeHandler = function() {
+		var win = $(window),
+			doc = $('html'),
+			resizeClass = 'resize-active',
+			flag, timer;
+		var removeClassHandler = function() {
+			flag = false;
+			doc.removeClass(resizeClass);
+		};
+		var resizeHandler = function() {
+			if(!flag) {
+				flag = true;
+				doc.addClass(resizeClass);
+			}
+			clearTimeout(timer);
+			timer = setTimeout(removeClassHandler, 500);
+		};
+		win.on('resize orientationchange', resizeHandler);
+	};
+
+	$.fn.mobileNav = function(options) {
+		return this.each(function() {
+			var params = $.extend({}, options, {container: this}),
+				instance = new MobileNav(params);
+			$.data(this, 'MobileNav', instance);
+		});
+	};
+}(jQuery));
+
+/*!
+ * SmoothScroll module
+ */
+;(function($, exports) {
+	// private variables
+	var page,
+		win = $(window),
+		activeBlock, activeWheelHandler,
+		wheelEvents = ('onwheel' in document || document.documentMode >= 9 ? 'wheel' : 'mousewheel DOMMouseScroll');
+
+	// animation handlers
+	function scrollTo(offset, options, callback) {
+		// initialize variables
+		var scrollBlock;
+		if (document.body) {
+			if (typeof options === 'number') {
+				options = { duration: options };
+			} else {
+				options = options || {};
+			}
+			page = page || $('html, body');
+			scrollBlock = options.container || page;
+		} else {
+			return;
+		}
+
+		// treat single number as scrollTop
+		if (typeof offset === 'number') {
+			offset = { top: offset };
+		}
+
+		// handle mousewheel/trackpad while animation is active
+		if (activeBlock && activeWheelHandler) {
+			activeBlock.off(wheelEvents, activeWheelHandler);
+		}
+		if (options.wheelBehavior && options.wheelBehavior !== 'none') {
+			activeWheelHandler = function(e) {
+				if (options.wheelBehavior === 'stop') {
+					scrollBlock.off(wheelEvents, activeWheelHandler);
+					scrollBlock.stop();
+				} else if (options.wheelBehavior === 'ignore') {
+					e.preventDefault();
+				}
+			};
+			activeBlock = scrollBlock.on(wheelEvents, activeWheelHandler);
+		}
+
+		// start scrolling animation
+		scrollBlock.stop().animate({
+			scrollLeft: offset.left,
+			scrollTop: offset.top
+		}, options.duration, function() {
+			if (activeWheelHandler) {
+				scrollBlock.off(wheelEvents, activeWheelHandler);
+			}
+			if ($.isFunction(callback)) {
+				callback();
+			}
+		});
+	}
+
+	// smooth scroll contstructor
+	function SmoothScroll(options) {
+		this.options = $.extend({
+			anchorLinks: 'a[href^="#"]',	// selector or jQuery object
+			container: null,		// specify container for scrolling (default - whole page)
+			extraOffset: null,		// function or fixed number
+			activeClasses: null,	// null, "link", "parent"
+			easing: 'swing',		// easing of scrolling
+			animMode: 'duration',	// or "speed" mode
+			animDuration: 800,		// total duration for scroll (any distance)
+			animSpeed: 1500,		// pixels per second
+			anchorActiveClass: 'anchor-active',
+			sectionActiveClass: 'section-active',
+			wheelBehavior: 'stop', // "stop", "ignore" or "none"
+			useNativeAnchorScrolling: false // do not handle click in devices with native smooth scrolling
+		}, options);
+		this.init();
+	}
+	SmoothScroll.prototype = {
+		init: function() {
+			this.initStructure();
+			this.attachEvents();
+		},
+		initStructure: function() {
+			this.container = this.options.container ? $(this.options.container) : $('html,body');
+			this.scrollContainer = this.options.container ? this.container : win;
+			this.anchorLinks = $(this.options.anchorLinks);
+		},
+		getAnchorTarget: function(link) {
+			// get target block from link href
+			var targetId = $(link).attr('href');
+			return $(targetId.length > 1 ? targetId : 'html');
+		},
+		getTargetOffset: function(block) {
+			// get target offset
+			var blockOffset = block.offset().top;
+			if (this.options.container) {
+				blockOffset -= this.container.offset().top - this.container.prop('scrollTop');
+			}
+
+			// handle extra offset
+			if (typeof this.options.extraOffset === 'number') {
+				blockOffset -= this.options.extraOffset;
+			} else if (typeof this.options.extraOffset === 'function') {
+				blockOffset -= this.options.extraOffset(block);
+			}
+			return { top: blockOffset };
+		},
+		attachEvents: function() {
+			var self = this;
+
+			// handle active classes
+			if (this.options.activeClasses) {
+				// cache structure
+				this.anchorData = [];
+				this.anchorLinks.each(function() {
+					var link = jQuery(this),
+						targetBlock = self.getAnchorTarget(link),
+						anchorDataItem;
+
+					$.each(self.anchorData, function(index, item) {
+						if (item.block[0] === targetBlock[0]) {
+							anchorDataItem = item;
+						}
+					});
+
+					if (anchorDataItem) {
+						anchorDataItem.link = anchorDataItem.link.add(link);
+					} else {
+						self.anchorData.push({
+							link: link,
+							block: targetBlock
+						});
+					}
+				});
+
+				// add additional event handlers
+				this.resizeHandler = function() {
+					self.recalculateOffsets();
+				};
+				this.scrollHandler = function() {
+					self.refreshActiveClass();
+				};
+
+				this.recalculateOffsets();
+				this.scrollContainer.on('scroll', this.scrollHandler);
+				win.on('resize', this.resizeHandler);
+			}
+
+			// handle click event
+			this.clickHandler = function(e) {
+				self.onClick(e);
+			};
+			if (!this.options.useNativeAnchorScrolling) {
+				this.anchorLinks.on('click', this.clickHandler);
+			}
+		},
+		recalculateOffsets: function() {
+			var self = this;
+			$.each(this.anchorData, function(index, data) {
+				data.offset = self.getTargetOffset(data.block);
+				data.height = data.block.outerHeight();
+			});
+			this.refreshActiveClass();
+		},
+		refreshActiveClass: function() {
+			var self = this,
+				foundFlag = false,
+				containerHeight = this.container.prop('scrollHeight'),
+				viewPortHeight = this.scrollContainer.height(),
+				scrollTop = this.options.container ? this.container.prop('scrollTop') : win.scrollTop();
+
+			// user function instead of default handler
+			if (this.options.customScrollHandler) {
+				this.options.customScrollHandler.call(this, scrollTop, this.anchorData);
+				return;
+			}
+
+			// sort anchor data by offsets
+			this.anchorData.sort(function(a, b) {
+				return a.offset.top - b.offset.top;
+			});
+			function toggleActiveClass(anchor, block, state) {
+				anchor.toggleClass(self.options.anchorActiveClass, state);
+				block.toggleClass(self.options.sectionActiveClass, state);
+			}
+
+			// default active class handler
+			$.each(this.anchorData, function(index) {
+				var reverseIndex = self.anchorData.length - index - 1,
+					data = self.anchorData[reverseIndex],
+					anchorElement = (self.options.activeClasses === 'parent' ? data.link.parent() : data.link);
+
+				if (scrollTop >= containerHeight - viewPortHeight) {
+					// handle last section
+					if (reverseIndex === self.anchorData.length - 1) {
+						toggleActiveClass(anchorElement, data.block, true);
+					} else {
+						toggleActiveClass(anchorElement, data.block, false);
+					}
+				} else {
+					// handle other sections
+					if (!foundFlag && (scrollTop >= data.offset.top - 1 || reverseIndex === 0)) {
+						foundFlag = true;
+						toggleActiveClass(anchorElement, data.block, true);
+					} else {
+						toggleActiveClass(anchorElement, data.block, false);
+					}
+				}
+			});
+		},
+		calculateScrollDuration: function(offset) {
+			var distance;
+			if (this.options.animMode === 'speed') {
+				distance = Math.abs(this.scrollContainer.scrollTop() - offset.top);
+				return (distance / this.options.animSpeed) * 1000;
+			} else {
+				return this.options.animDuration;
+			}
+		},
+		onClick: function(e) {
+			var targetBlock = this.getAnchorTarget(e.currentTarget),
+				targetOffset = this.getTargetOffset(targetBlock);
+
+			e.preventDefault();
+			scrollTo(targetOffset, {
+				container: this.container,
+				wheelBehavior: this.options.wheelBehavior,
+				duration: this.calculateScrollDuration(targetOffset)
+			});
+			this.makeCallback('onChange', this);
+		},
+		makeCallback: function(name) {
+			if (typeof this.options[name] === 'function') {
+				var args = Array.prototype.slice.call(arguments);
+				args.shift();
+				this.options[name].apply(this, args);
+			}
+		},
+		destroy: function() {
+			if (this.options.activeClasses) {
+				win.off('resize', this.resizeHandler);
+				this.scrollContainer.off('scroll', this.scrollHandler);
+			}
+			this.anchorLinks.off('click', this.clickHandler);
+		}
+	};
+
+	// public API
+	$.extend(SmoothScroll, {
+		scrollTo: function(blockOrOffset, durationOrOptions, callback) {
+			scrollTo(blockOrOffset, durationOrOptions, callback);
+		}
+	});
+
+	// export module
+	exports.SmoothScroll = SmoothScroll;
+}(jQuery, this));
+
+/*
+ * Image Stretch module
+ */
+var ImageStretcher = {
+	getDimensions: function(data) {
+		// calculate element coords to fit in mask
+		var ratio = data.imageRatio || (data.imageWidth / data.imageHeight),
+			slideWidth = data.maskWidth,
+			slideHeight = slideWidth / ratio;
+
+		if(slideHeight < data.maskHeight) {
+			slideHeight = data.maskHeight;
+			slideWidth = slideHeight * ratio;
+		}
+		return {
+			width: slideWidth,
+			height: slideHeight,
+			top: (data.maskHeight - slideHeight) / 2,
+			left: (data.maskWidth - slideWidth) / 2
+		};
+	},
+	getRatio: function(image) {
+		if(image.prop('naturalWidth')) {
+			return image.prop('naturalWidth') / image.prop('naturalHeight');
+		} else {
+			var img = new Image();
+			img.src = image.prop('src');
+			return img.width / img.height;
+		}
+	},
+	imageLoaded: function(image, callback) {
+		var self = this;
+		var loadHandler = function() {
+			callback.call(self);
+		};
+		if(image.prop('complete')) {
+			loadHandler();
+		} else {
+			image.one('load', loadHandler);
+		}
+	},
+	resizeHandler: function() {
+		var self = this;
+		jQuery.each(this.imgList, function(index, item) {
+			if(item.image.prop('complete')) {
+				self.resizeImage(item.image, item.container);
+			}
+		});
+	},
+	resizeImage: function(image, container) {
+		this.imageLoaded(image, function() {
+			var styles = this.getDimensions({
+				imageRatio: this.getRatio(image),
+				maskWidth: container.width(),
+				maskHeight: container.height()
+			});
+			image.css({
+				width: styles.width,
+				height: styles.height,
+				marginTop: styles.top,
+				marginLeft: styles.left
+			});
+		});
+	},
+	add: function(options) {
+		var container = jQuery(options.container ? options.container : window),
+			image = typeof options.image === 'string' ? container.find(options.image) : jQuery(options.image);
+
+		// resize image
+		this.resizeImage(image, container);
+
+		// add resize handler once if needed
+		if(!this.win) {
+			this.resizeHandler = jQuery.proxy(this.resizeHandler, this);
+			this.imgList = [];
+			this.win = jQuery(window);
+			this.win.on('resize orientationchange', this.resizeHandler);
+		}
+
+		// store item in collection
+		this.imgList.push({
+			container: container,
+			image: image
+		});
+	}
+};
+
+/*!
+* FitVids 1.0.3
+*
+* Copyright 2013, Chris Coyier - http://css-tricks.com + Dave Rupert - http://daverupert.com
+* Credit to Thierry Koblentz - http://www.alistapart.com/articles/creating-intrinsic-ratios-for-video/
+* Released under the WTFPL license - http://sam.zoy.org/wtfpl/
+*
+* Date: Thu Sept 01 18:00:00 2011 -0500
+*/
+;(function(a){a.fn.fitVids=function(b){var c={customSelector:null};if(!document.getElementById("fit-vids-style")){var f=document.createElement("div"),d=document.getElementsByTagName("base")[0]||document.getElementsByTagName("script")[0],e="&shy;<style>.fluid-width-video-wrapper{width:100%;position:relative;padding:0;}.fluid-width-video-wrapper iframe,.fluid-width-video-wrapper object,.fluid-width-video-wrapper embed {position:absolute;top:0;left:0;width:100%;height:100%;}</style>";f.className="fit-vids-style";f.id="fit-vids-style";f.style.display="none";f.innerHTML=e;d.parentNode.insertBefore(f,d)}if(b){a.extend(c,b)}return this.each(function(){var g=["iframe[src*='player.vimeo.com']","iframe[src*='youtube.com']","iframe[src*='youtube-nocookie.com']","iframe[src*='kickstarter.com'][src*='video.html']","object","embed"];if(c.customSelector){g.push(c.customSelector)}var h=a(this).find(g.join(","));h=h.not("object object");h.each(function(){var m=a(this);if(this.tagName.toLowerCase()==="embed"&&m.parent("object").length||m.parent(".fluid-width-video-wrapper").length){return}var i=(this.tagName.toLowerCase()==="object"||(m.attr("height")&&!isNaN(parseInt(m.attr("height"),10))))?parseInt(m.attr("height"),10):m.height(),j=!isNaN(parseInt(m.attr("width"),10))?parseInt(m.attr("width"),10):m.width(),k=i/j;if(!m.attr("id")){var l="fitvid"+Math.floor(Math.random()*999999);m.attr("id",l)}m.wrap('<div class="fluid-width-video-wrapper"></div>').parent(".fluid-width-video-wrapper").css("padding-top",(k*100)+"%");m.removeAttr("height").removeAttr("width")})})}})(window.jQuery||window.Zepto);
